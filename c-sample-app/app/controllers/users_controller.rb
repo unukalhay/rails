d7afdc4034231c762adf75185a17ab48e961d3ae
unukalhay <unukalhay@gmail.com>
2019-02-16 03:59:47 +0000

    Implement basic login

--- c-sample-app/app/controllers/users_controller.rb
+++ c-sample-app/app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)  # or just 'redirect_to @user'
     else
--- c-sample-app/app/models/user.rb
+++ c-sample-app/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
@@ -12,5 +13,13 @@ class User < ApplicationRecord
             
   validates :password, presence: true, length: { minimum: 6 }
   
-  has_secure_password
+  
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  
 end
