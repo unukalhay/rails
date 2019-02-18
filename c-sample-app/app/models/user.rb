7d8817638e8b49b877dca13a766f7169c0ee6490
unukalhay <unukalhay@gmail.com>
2019-02-18 23:31:09 +0000

    Implement advanced login

--- c-sample-app/app/models/user.rb
+++ c-sample-app/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
+  attr_accessor :remember_token
+  
   
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
@@ -11,6 +12,8 @@ class User < ApplicationRecord
             format: {with: VALID_EMAIL_REGEX}, 
             uniqueness: { case_sensitive: false }
             
+  has_secure_password
+
   validates :password, presence: true, length: { minimum: 6 }
   
   
@@ -21,5 +24,26 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
   
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+   # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
   
 end
--- c-sample-app/test/models/user_test.rb
+++ c-sample-app/test/models/user_test.rb
@@ -80,4 +80,7 @@ class UserTest < ActiveSupport::TestCase
   end
   
  
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end
 end
